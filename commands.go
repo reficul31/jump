02aecdca755b2b15becf8ce925467124a997737a
reficul31 <reficul31@gmail.com>
2017-07-03 08:47:14 +0530

    Adding the basic commands

---
+++ commands.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+  "fmt"
+  "github.com/reficul31/jump/app"
+  "github.com/urfave/cli"
+  "path/filepath"
+  "os"
+)
+
+var err error
+
+func PopulateCommands(jump *cli.App) {
+  jump.Action = func(c *cli.Context) {
+    path, err := app.FetchCheckpoint(c.Args().Get(0))
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+    fmt.Println(path)
+    return
+  }
+
+  jump.Commands = []cli.Command{
+    {
+      Name: "add",
+      Aliases: []string{"a"},
+      Usage: "Add a checkpoint to jump",
+      Action: func(c *cli.Context) {
+        dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+        if err != nil {
+          fmt.Println("cannot find the current directory")
+          return
+        }
+        err = app.AddCheckpoint(c.Args().First(), dir)
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+        return
+      },
+    },
+    {
+      Name: "rm",
+      Aliases: []string{"r"},
+      Usage: "Remove a checkpoint",
+      Action: func(c *cli.Context) {
+        err = app.RemoveCheckpoint(c.Args().First())
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+        return
+      },
+    },
+    {
+      Name: "show",
+      Aliases: []string{"s"},
+      Usage: "Show all the saved checkpoints",
+      Action: func(c *cli.Context) {
+        err = app.ShowCheckpoints()
+        if err != nil {
+          fmt.Println(err)
+          return
+        }
+        return
+      },
+    },
+  }
+}
