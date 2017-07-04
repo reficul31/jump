8a73aa6b2d4517abe67ff5145c0252f1d4b0fc8e
reficul31 <reficul31@gmail.com>
2017-07-04 19:52:40 +0530

    Adding description

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
+/*
+############################ JUMP ############################
+    App Name         : Jump
+    Type             : CLI
+    Language         : Golang
+    Description      : A thin wrapper around the "cd"
+                       command in the bash script. This CLI
+                       allows us to make checkpoints in the
+                       filesystem and move about much more
+                       easily and efficiently.
+    Version          : 0.0.1
+    Packages Used    : (
+        github.com/urfave/cli
+        github.com/olekukonko/tablewriter
+        github.com/syndtr/goleveldb/leveldb
+    )
+    Insipred By      : Teleport(https://github.com/bollu/teleport)
+    Original Author  : Bollu(https://github.com/bollu)
+    Author           : Shivang(https://github.com/reficul31)
+
+############################ JUMP ############################
+
+*/
+
 package main
 
 import (
