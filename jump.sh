708b5599644ccab5ab432f25f641017f95d9b91e
reficul31 <reficul31@gmail.com>
2017-07-05 10:17:37 +0530

    Changing the callname

--- jump.sh
+++ jump.sh
@@ -2,7 +2,7 @@
 # Transmuted from https://github.com/bollu/teleport/blob/master/teleport.sh
 # Changes directory path if the exit code is 2, otherwise print the statement on the console
 
-function jmp() {
+function jp() {
     TRAIL=`jump $@`
     if [ $? -eq 2 ]
         then cd "$TRAIL"
